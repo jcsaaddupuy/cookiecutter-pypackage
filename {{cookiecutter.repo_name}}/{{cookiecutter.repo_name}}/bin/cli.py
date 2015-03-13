6e9d517a95861584e4c9091f6ece8d78094c7df0
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-03-13 14:05:37 +0100

    added logging and argparse to entrypoint

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/bin/cli.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/bin/cli.py
@@ -1,16 +1,40 @@
 # coding: utf-8
 
+import argparse
 import logging
 logger = logging.getLogger(__name__)
 
+
 class App(object):
+
+    """ {{cookiecutter.repo_name}} main entry point class"""
+
     def __init__(self):
-        pass
+        self.args_parser = argparse.ArgumentParser()
+
+    def _init_args(self):
+        self.args_parser.add_argument('-d', '--debug',
+                                      dest='debug',
+                                      help='Enable debug mode',
+                                      action='store_true'
+                                      )
+
+    def _configure_loggers(self, args):
+        """ {{cookiecutter.repo_name}} loggers configuration"""
+        if args.debug:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
 
     def main(self):
-        print "{{cookiecutter.repo_name}} up and running"
+        """ {{cookiecutter.repo_name}} main entry point method"""
+        self._init_args()
+        args = self.args_parser.parse_args()
+        self._configure_loggers(args)
+
+        logger.info("{{cookiecutter.repo_name}} up and running")
+
 
 def main():
+    """ {{cookiecutter.repo_name}} main entry point method called in setup.py"""
     App().main()
-
-
